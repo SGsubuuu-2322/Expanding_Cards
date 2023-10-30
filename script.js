e724d5251550dba84bb826bd775957bc4bac82e4
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-10-30 17:44:46 +0530

    Added the meaningful comments for script file of this project...

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const panels = document.querySelectorAll(".panel");
 
+// This script is written for adding active class dynamically in the panel when clicked...
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClass();
@@ -7,6 +8,7 @@ panels.forEach((panel) => {
   });
 });
 
+// This script is written for removing all active class from all the panel except the current one which is clicked...
 function removeActiveClass() {
   panels.forEach((panel) => {
     panel.classList.remove("active");
